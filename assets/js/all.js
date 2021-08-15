33835bef2f937f883822cf4ecd95bf9bb180b104
LoKing23 <mongrock30102@gmail.comm>
2021-08-15 09:39:26 +0800

    Update 2021-08-15T01:39:18.990Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,9 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); //inline editor
+
+InlineEditor.create(document.querySelector('#editor'))["catch"](function (error) {
+  console.error(error);
 });
 //# sourceMappingURL=all.js.map
