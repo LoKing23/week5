fced2203e4b42aa4c7550f547fa97173420d2c76
LoKing23 <mongrock30102@gmail.comm>
2021-08-15 09:40:05 +0800

    我也不知道做到哪，剩下admin的的modal

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,9 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+//inline editor
+InlineEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
